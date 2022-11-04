b55f5e854047d22cada057fc2cdc56a921bdc440
Jon Elorriaga <jonelorriaga@iMac-de-Jon.local>
2022-11-04 14:35:52 +0100

    0.81

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-      <BrowserRouter>
-        <Routes className="App">
-          <Route path='/' exact element={<Home/>}/>
-          <Route path='/bolsa' exact element={<Bolsa/>}/>
-        </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Routes className="App">
+        <Route path='/' exact element={<Home/>}/>
+        <Route path='/bolsa' exact element={<Bolsa/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
